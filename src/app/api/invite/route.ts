c1ee1e27949c837ddeb9e84062ff31a7a6b81428
Hari-ghm <hmadhav2006@gmail.com>
2026-01-06 09:24:44 +0530

    enabled launch pass

--- src/app/api/invite/route.ts
+++ src/app/api/invite/route.ts
@@ -2,7 +2,7 @@ import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 import clientPromise from "@/lib/mongodb";
 
-const MAX_LAUNCH_PASSES = 150;
+const MAX_LAUNCH_PASSES = 200;
 // hello
 //test2
 export async function GET() {
